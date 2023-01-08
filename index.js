1881294d76cea72ba8093d61aebadaf2b5a22cfb
Anyi79 <ajfr79@gmail.com>
2023-01-08 22:01:11 +0100

    Completed form

--- index.js
+++ index.js
@@ -1,47 +1,79 @@
 //event listener
 document.querySelector("#btn-add").addEventListener("click", add);
-/* document.querySelector("#btn-reset").addEventListener("click", reset);
-document.querySelector("#delate-last").addEventListener("click",delate-last);
- */
 
-//modelo de datos
+
+//variables del array
+
 let names = [];
-/* let surnames = [];
-let course = [];
-let sclass = []; */
+let surnames = [];
+let courses = [];
+let sclasses = [];
 
+//función agregar un elemento al array
 
 function add() {
-    let name = document.querySelector('#name').value;
-    let surname = document.querySelector('#surname').value;
-    let course = document.querySelector('#course').value;
-    let sclass = document.querySelector('#sclass').value;
 
-    //agrego un elemento al arreglo
-    names.push(name);
-    names.push(surname);
-    names.push(course);
-    names.push(sclass);
-    console.log(names);
+  let name = document.querySelector('#name').value;
+  let surname = document.querySelector('#surname').value;
+  let course = document.querySelector('#course').value;
+  let sclass = document.querySelector('#sclass').value;
 
-    //muestro el arreglo
-    show();
 
-    //vaciar el imput
-    document.querySelector('#name').value = "";
-    document.querySelector('#surname').value = "";
-    document.querySelector('#course').value = "";
-    document.querySelector('#sclass').value = "";
+  names.push(name);
+  surnames.push(surname);
+  courses.push(course);
+  sclasses.push(sclass);
 
-    
+
+  //muestra el array
+  show();
+
+  // vacio el imput
+  document.querySelector('#name').value = "";
+  document.querySelector('#surname').value = "";
+  document.querySelector('#course').value = "";
+  document.querySelector('#sclass').value = "";
+ 
+
 }
+//función mostrar array
 
 function show() {
-    let list = document.querySelector('#list');
-    list.innerHTML = "";
+  let listn = document.querySelector('#name');
+  let lists = document.querySelector('#surname');
+  let listc = document.querySelector('#course');
+  let listsc = document.querySelector('#sclass');
 
-    for (let actualDate of names) {
-        list.innerHTML += "<li>" + actualDate + "</li>";
-    }
-    
+
+  listn.innerHTML = "";
+  lists.innerHTML = "";
+  listc.innerHTML = "";
+  listsc.innerHTML = "";
+
+  //Agregar filas a la tabla con los datos ingresados
+
+  let dateTable = document.getElementById("dateTable");
+  let newDateRow = dateTable.insertRow(-1);
+
+  let newDateCell = newDateRow.insertCell(0);
+  newDateCell.textContent = document.querySelector('#name').value;
+
+  newDateCell = newDateRow.insertCell(1);
+  newDateCell.textContent = document.querySelector('#surname').value;
+
+  newDateCell = newDateRow.insertCell(2);
+  newDateCell.textContent = document.querySelector('#course').value;
+
+  newDateCell = newDateRow.insertCell(3);
+  newDateCell.textContent = document.querySelector('#sclass').value;
+
+  let newDeleteCell = newDateRow.insertCell(4);
+  let deleteButton = document.createElement("button");
+  deleteButton.textContent = "Delete";
+  newDeleteCell.appendChild(deleteButton);
+
+  deleteButton.addEventListener("click", (event) => {
+    event.target.parentNode.parentNode.remove()
+   });
+ 
 }
